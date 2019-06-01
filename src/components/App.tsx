dcf3094df9cbc4daa42f850f9029959008c35bee
Henry <henry.chou@tuta.io>
2019-06-01 23:02:17 +0800

    finish header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import Header from './Header';
+import BookList from './BookList';
 
 const App = props => {
   return (
-    <div className="container-fluid">
-      <Header />
+    <div className="bg-light" style={{ minHeight: '100vh' }}>
+      <div className="container-fluid w-75">
+        <Header />
+      </div>
     </div>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,18 @@ import * as React from 'react';
 const Header = props => {
   return (
     <div className="header">
-      <h1>Header</h1>
+      <h3 className="py-3 text-center text-primary">
+        好想工作室 - 天瓏書局書單搜尋
+      </h3>
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="輸入書名"
+          aria-label="enter book title"
+          aria-describedby="book search"
+        />
+      </div>
     </div>
   );
 };
