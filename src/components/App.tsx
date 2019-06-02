67843ff2a079097fb933913847f47c5030d37acf
Henry <henry.chou@tuta.io>
2019-06-02 15:12:10 +0800

    move the logic to App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,37 @@
 import * as React from 'react';
 import Header from './Header';
 import BookList from './BookList';
+import { List } from 'models/list';
+import Book from './Book';
 
 const App = props => {
+  const [list, setList] = React.useState<List[]>([]);
+  const fetchBookList = async () => {
+    try {
+      const res = await fetch('https://bookshelf.goodideas-studio.com/api');
+      if (res.ok) {
+        const data = await res.json();
+        const list = data.list;
+        setList(list);
+      } else {
+        throw Error('Fail to fetch data.');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+  React.useEffect(() => {
+    fetchBookList();
+  }, [list]);
   return (
     <div className="bg-light" style={{ minHeight: '100vh' }}>
       <div className="container-fluid" style={{ maxWidth: '60%' }}>
         <Header />
-        <BookList />
+        <BookList
+          render={() => {
+            return list.map(item => <Book key={item.ISBN} book={item} />);
+          }}
+        />
       </div>
     </div>
   );
--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,37 +1,14 @@
 import * as React from 'react';
-import { List } from 'models/list';
-import Book from './Book';
 
-const BookList = props => {
-  const [list, setList] = React.useState<List[]>([]);
-  const fetchBookList = async () => {
-    try {
-      const res = await fetch('https://bookshelf.goodideas-studio.com/api');
-      if (res.ok) {
-        const data = await res.json();
-        const list = data.list;
-        setList(list);
-      } else {
-        throw Error('Fail to fetch data.');
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  };
-  React.useEffect(() => {
-    fetchBookList();
-  }, [list]);
+interface BookListProps {
+  render: () => JSX.Element[];
+}
+
+const BookList = (props: BookListProps) => {
+  const { render } = props;
   return (
     <div className="book-list">
-      <div className="card-columns">
-        {!list.length ? (
-          <div>讀取中...</div>
-        ) : (
-          list.map(item => {
-            return <Book book={item} key={item.ISBN} />;
-          })
-        )}
-      </div>
+      <div className="card-columns">{render()}</div>
     </div>
   );
 };
