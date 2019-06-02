bbf002273d31e7334438950e0d5bfc090bda9a34
Henry <henry.chou@tuta.io>
2019-06-02 18:21:01 +0800

    add branch gh-pages for static site

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './components/App';
-import 'normalize.css/normalize.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/main.scss';
-
-ReactDOM.render(<App />, document.getElementById('root'));
